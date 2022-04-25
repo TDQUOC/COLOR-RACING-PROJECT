2b13280ffcc12e79cdb1e1eb001ce7d1b03a8c14
TDQ <tdq17072000@gmail.com>
2022-04-25 14:56:11 +0700

    Remove WindZone, Complete LapCount, Save BestTime, SkyBox

--- Assets/Script/LapComplete.cs
+++ Assets/Script/LapComplete.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ea3c95ff05167724571c6d63b6a22674c54f7ab9aeca3f63e09dd1954e2db46
-size 1467
+oid sha256:3c08a51581422d74c1f08bba3cc18126c083e4ba2e412ca28b99962b72423c0a
+size 2064
