d8a79d659c83125166a0a058ff8bf9ec3f0aa2be
TDQ <tdq17072000@gmail.com>
2022-04-25 11:11:33 +0700

    LapCount, WindZone

--- Assets/Script/LapComplete.cs
+++ Assets/Script/LapComplete.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0926f841d9471d2b3990f19d3d9e6a647bcb0316c601cdcc43a91bc64d1623ae
-size 1339
+oid sha256:8ea3c95ff05167724571c6d63b6a22674c54f7ab9aeca3f63e09dd1954e2db46
+size 1467
